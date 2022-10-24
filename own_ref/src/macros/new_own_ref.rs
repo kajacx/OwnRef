81e1e98d43521e1ffb79d2d397ea45370eb624cb
KarelHrkal <karel.hrkal@ibrconsulting.cz>
2022-10-24 15:01:09 +0200

    Rename internal lifetime scope marker

--- own_ref/src/macros/new_own_ref.rs
+++ own_ref/src/macros/new_own_ref.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! new_own_ref {
     (mut $ref_name:ident, $data_variable:ident) => {
-        let _phantom = ();
+        let _own_ref_phantom_scope = ();
         let mut $ref_name = unsafe {
             // SAFETY:
             // We know the OwnRef will not outlive the current stack,
-            // because of the _phantom lifetime.
+            // because of the _own_ref_phantom_scope lifetime.
             // Also, the data cannot cannot be used after "moving it" into the OwnRef
             // because we forget it immediately.
             // Finally, the data will not be deallocated twice,
@@ -13,17 +13,17 @@ macro_rules! new_own_ref {
             #[allow(deprecated)]
             own_ref::internals::new_own_ref_mut(
                 &mut $data_variable,
-                own_ref::internals::lifetime_of(&_phantom),
+                own_ref::internals::lifetime_of(&_own_ref_phantom_scope),
             )
         };
         std::mem::forget($data_variable);
     };
     ($ref_name:ident, $data_variable:ident) => {
-        let _phantom = ();
+        let _own_ref_phantom_scope = ();
         let $ref_name = unsafe {
             // SAFETY:
             // We know the OwnRef will not outlive the current stack,
-            // because of the _phantom lifetime.
+            // because of the _own_ref_phantom_scope lifetime.
             // Also, the data cannot cannot be used after "moving it" into the OwnRef
             // because we forget it immediately.
             // Finally, the data will not be deallocated twice,
@@ -31,11 +31,9 @@ macro_rules! new_own_ref {
             #[allow(deprecated)]
             own_ref::internals::new_own_ref(
                 &$data_variable,
-                own_ref::internals::lifetime_of(&_phantom),
+                own_ref::internals::lifetime_of(&_own_ref_phantom_scope),
             )
         };
         std::mem::forget($data_variable);
     };
 }
-
-// pub(crate) use new_own_ref; TODO: remove for good?
